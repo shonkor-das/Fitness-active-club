00ad57e10295bf80bf18972f4347dce5aa6b0d9f
Shonkor Das <94380595+shonkor-das@users.noreply.github.com>
2022-09-29 23:04:33 +0600

    person section added

--- src/components/Break/Break.js
+++ src/components/Break/Break.js
@@ -1,17 +1,23 @@
 
 import './Break.css';
 
-const Break = () => {
 
+const Break = () => {
+    function handleClick(e) {
+
+        e.preventDefault();
+        console.log('click button');
+  
+      }
     return (
         <div>
             <h4>Add A Break</h4>
             <div className='break-info'>
-                <button className='btn-break' >10s</button>
-                <button className='btn-break' >20s</button >
-                <button className='btn-break' >30s</button>
-                <button className='btn-break' >40s</button >
-                <button className='btn-break' >50s</button >
+                <button className='btn-break' onClick={handleClick}>10s</button>
+                <button className='btn-break' onClick={handleClick}>20s</button >
+                <button className='btn-break' onClick={handleClick}>30s</button>
+                <button className='btn-break' onClick={handleClick}>40s</button >
+                <button className='btn-break' onClick={handleClick}>50s</button >
             </div>
         </div>
     );
